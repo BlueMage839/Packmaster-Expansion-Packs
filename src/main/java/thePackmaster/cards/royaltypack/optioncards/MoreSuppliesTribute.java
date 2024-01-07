6eafa977e4f32515cd36d083e955b1b5875a3aa3
Cláuvin <Clauvin@users.noreply.github.com>
2024-01-07 17:36:24 -0300

    MoreSupplies now uses magicNumber instead of TRIBUTE_GOLD_AMOUNT

--- src/main/java/thePackmaster/cards/royaltypack/optioncards/MoreSuppliesTribute.java
+++ src/main/java/thePackmaster/cards/royaltypack/optioncards/MoreSuppliesTribute.java
@@ -16,12 +16,12 @@ import static thePackmaster.SpireAnniversary5Mod.makeID;
 public class MoreSuppliesTribute extends AbstractRoyaltyCard {
 
     public final static String ID = makeID("MoreSuppliesTribute");
-    public static int TRIBUTE_GOLD_AMOUNT;
 
     public MoreSuppliesTribute(int tribute_gold_amount){
         super(ID, -2, CardType.STATUS, CardRarity.SPECIAL, CardTarget.SELF, ThePackmaster.Enums.PACKMASTER_RAINBOW,
                 "anniv5Resources/images/cards/OptionTribute.png");
-        TRIBUTE_GOLD_AMOUNT = tribute_gold_amount;
+        baseMagicNumber = tribute_gold_amount;
+        magicNumber = baseMagicNumber;
     }
 
     @Override
@@ -37,7 +37,7 @@ public class MoreSuppliesTribute extends AbstractRoyaltyCard {
 
     @Override
     public void onChoseThisOption() {
-        Wiz.atb(new PayTributeAction(TRIBUTE_GOLD_AMOUNT));
+        Wiz.atb(new PayTributeAction(magicNumber));
         for (int i = 0; i < AbstractDungeon.player.potionSlots; i++){
             Wiz.atb(new ObtainPotionAction(AbstractDungeon.returnRandomPotion(false)));
         }
