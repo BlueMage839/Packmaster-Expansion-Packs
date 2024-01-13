28f7ebb3841aff17503cd9aed4b587292de96a6f
Cláuvin <Clauvin@users.noreply.github.com>
2024-01-13 11:35:10 -0300

    Nerfed Throw Soulstones

--- src/main/java/thePackmaster/cards/royaltypack/ThrowSoulstones.java
+++ src/main/java/thePackmaster/cards/royaltypack/ThrowSoulstones.java
@@ -1,7 +1,14 @@
 package thePackmaster.cards.royaltypack;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.BlizzardEffect;
 import thePackmaster.actions.royaltypack.TributeOrAusterityAction;
 import thePackmaster.cards.royaltypack.optioncards.ThrowSoulstonesAusterity;
 import thePackmaster.cards.royaltypack.optioncards.ThrowSoulstonesTribute;
@@ -12,29 +19,35 @@ import static thePackmaster.SpireAnniversary5Mod.makeID;
 public class ThrowSoulstones extends AbstractRoyaltyCard {
 
     public final static String ID = makeID("ThrowSoulstones");
-    public final static int TRIBUTE_DAMAGE = 15;
-    public final static int AUSTERITY_DAMAGE = 5;
+    public final static int TRIBUTE_DAMAGE = 12;
+    public final static int AUSTERITY_DAMAGE = 18;
 
 
     public ThrowSoulstones(){
-        super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        super(ID, 1, CardType.ATTACK, CardRarity.RARE, CardTarget.ALL_ENEMY);
         baseDamage = damage = TRIBUTE_DAMAGE;
         baseSecondDamage = secondDamage = AUSTERITY_DAMAGE;
     }
 
     @Override
     public void upp() {
-        this.upgradeDamage(5);
+        this.upgradeDamage(3);
+        this.upgradeSecondDamage(3);
     }
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        AbstractRoyaltyCard tsTributeChoiceCard = new ThrowSoulstonesTribute(this, damage);
-        AbstractRoyaltyCard tsAusterityChoiceCard = new ThrowSoulstonesAusterity(secondDamage);
-        for (int i = 0; i < this.timesUpgraded; i++){
-            tsTributeChoiceCard.upgrade();
-            tsAusterityChoiceCard.upgrade();
+        if (Settings.FAST_MODE) {
+            this.addToBot(new VFXAction(new BlizzardEffect(damage, AbstractDungeon.getMonsters().shouldFlipVfx()), 0.25F));
+        } else {
+            this.addToBot(new VFXAction(new BlizzardEffect(damage, AbstractDungeon.getMonsters().shouldFlipVfx()), 0.5F));
         }
+        int[] damageMatrix = DamageInfo.createDamageMatrix(this.baseDamage, false);
+        DamageAllEnemiesAction dmgAll = new DamageAllEnemiesAction(AbstractDungeon.player, damageMatrix, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.NONE);
+        addToBot(dmgAll);
+
+        AbstractRoyaltyCard tsTributeChoiceCard = new ThrowSoulstonesTribute(this, secondDamage);
+        AbstractRoyaltyCard tsAusterityChoiceCard = new ThrowSoulstonesAusterity();
 
         Wiz.atb(new TributeOrAusterityAction(tsTributeChoiceCard, tsAusterityChoiceCard));
     }
